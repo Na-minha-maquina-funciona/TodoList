210ec529e327b5406fce93384dd0fb481029996a
GabrielSiqueiraGama <siqueiragamagabriel@gmai.com>
2023-05-26 15:24:41 -0300

    TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,14 +3,55 @@ import TodoForm from './TodoForm';
 import Todo from './Todo';
 
 function TodoList() {
+  const [todos, setTodos] = useState([]);
 
-    return (
-        <>
-            <h1>Quais seus planos para hoje?</h1>
-            <TodoForm />
-            <Todo />
-        </>
-    );
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
 }
 
 export default TodoList;
